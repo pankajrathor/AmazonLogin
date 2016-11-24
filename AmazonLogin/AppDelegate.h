37bf2d41a2e759b6091c6ca53159a727618141e5
Pankaj Rathor <pankaj.rathor@capgemini.com>
2016-11-24 14:34:05 +0100

    Initial Commit

---
+++ AmazonLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AmazonLogin
+//
+//  Created by Pankaj Rathor on 24/11/16.
+//  Copyright © 2016 Sogeti B.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AmazonLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AmazonLogin
+//
+//  Created by Pankaj Rathor on 24/11/16.
+//  Copyright © 2016 Sogeti B.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
